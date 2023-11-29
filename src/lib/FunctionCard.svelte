a520c6478653d0b37d576ac0257144d3e5f875ee
Laen <laen@oshpark.com>
2023-11-28 22:41:26 -0800

    Proto-functions.

---
+++ src/lib/FunctionCard.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	export let fun;
+	let open = true;
+</script>
+
+<button id="myBtn" on:click={() => (open = true)}>Open Modal</button>
+
+<!-- The Modal -->
+<dialog {open} class="absolute top-0 mt-20">
+	<!-- Modal content -->
+	<div class="modal-content">
+		<span class="close" on:click={() => (open = false)}>&times;</span>
+
+		<h1>Function</h1>
+		<button value="cancel" formmethod="dialog">Close</button>
+
+		<form class="">
+			<label for="name">Name</label>
+			<input id="name" bind:value={fun.name} />
+
+			<label for="description">Description</label>
+			<input id="description" bind:value={fun.description} />
+
+			<label for="location">Location</label>
+			<input id="location" bind:value={fun.parameters.properties.location.description} />
+
+			<label for="unit">Unit</label>
+			<select id="unit" bind:value={fun.parameters.properties.unit.type}>
+				<option value="celsius">Celsius</option>
+				<option value="fahrenheit">Fahrenheit</option>
+			</select>
+		</form>
+	</div>
+</dialog>
+
+<style>
+	form > input {
+		display: block;
+		margin-bottom: 1rem;
+		color: #333;
+	}
+</style>
---
+++ src/lib/FunctionModule.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let functions = [];
+	import FunctionCard from './FunctionCard.svelte';
+	$: console.log({ functions });
+
+	function defaultFunction() {
+		return {
+			name: 'get_current_weather',
+			description: 'Get the current weather in a given location',
+			parameters: {
+				type: 'object',
+				properties: {
+					location: {
+						type: 'string',
+						description: 'The city and state, e.g. San Francisco, CA'
+					},
+					unit: { type: 'string', enum: ['celsius', 'fahrenheit'] }
+				},
+				required: ['location']
+			}
+		};
+	}
+</script>
+
+<div class="function-module">
+	{#each functions as fun}
+		<FunctionCard bind:fun />
+	{/each}
+	<button class="add-function" on:click={() => (functions = [...functions, defaultFunction()])}
+		>Add Function</button
+	>
+</div>
